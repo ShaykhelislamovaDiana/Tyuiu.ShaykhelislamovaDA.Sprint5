fded16622fb980e4cdd8731190998ad1bfea6b89
Диана Шайхелисламова <Tyuiu.ShaykhelislamovaDA@outlook.com>
2025-11-03 21:38:18 +0500

    Task0

--- Tyuiu.ShaykhelislamovaDA.Sprint5.Task0.V11.Lib/DataService.cs
+++ Tyuiu.ShaykhelislamovaDA.Sprint5.Task0.V11.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.ShaykhelislamovaDA.Sprint5.Task0.V11.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            string path = Path.GetTempFileName();
             double y = Math.Round((4.0 - Math.Pow((double)x, 3)) / Math.Pow((double)x, 2), 3);
             File.WriteAllText(path, y.ToString());
             return path;
