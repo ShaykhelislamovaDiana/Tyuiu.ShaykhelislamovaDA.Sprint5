781d992f7dfd688be5d4eb1410220ba2abd71b46
Диана Шайхелисламова <Tyuiu.ShaykhelislamovaDA@outlook.com>
2025-11-15 14:26:17 +0500

    Task4

---
+++ Tyuiu.ShaykhelislamovaDA.Sprint5.Task4.V19.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.ShaykhelislamovaDA.Sprint5.Task4.V19.Lib
+{
+    public class DataService : ISprint5Task4V19
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            strX = strX.Replace('.', ',');
+            double x = Convert.ToDouble(strX);
+            double res = Math.Round(Math.Pow(x/Math.Cos(x), 2), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ShaykhelislamovaDA.Sprint5.Task4.V19/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.ShaykhelislamovaDA.Sprint5.Task4.V19.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выолнила: Шайхелисламова Д. А. | ПКТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Двумерные массивы  (статический ввод)                                   *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #19                                                             *");
+        Console.WriteLine("* Выполнила: Шайхелисламова Диана Артуровна | ПКТБ-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+        Console.WriteLine("* значениями с клавиатуры в диапазоне от 3 до 6. Найдите сумму нечетных   *");
+        Console.WriteLine("* элементов массива.                                                      *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask4V19.txt";
+
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Данные находятся в файле: " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.LoadFromDataFile(path));
+    }
+}
