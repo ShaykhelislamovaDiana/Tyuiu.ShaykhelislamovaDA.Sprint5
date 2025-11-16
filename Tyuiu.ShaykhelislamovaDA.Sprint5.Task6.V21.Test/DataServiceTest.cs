b1810fce0fa7a89b14414979332fc211d809bad8
Диана Шайхелисламова <Tyuiu.ShaykhelislamovaDA@outlook.com>
2025-11-16 13:51:03 +0500

    Task6

---
+++ Tyuiu.ShaykhelislamovaDA.Sprint5.Task6.V21.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.ShaykhelislamovaDA.Sprint5.Task6.V21.Lib;
+namespace Tyuiu.ShaykhelislamovaDA.Sprint5.Task6.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.ShaykhelislamovaDA.Sprint5.Task6.V21/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.ShaykhelislamovaDA.Sprint5.Task6.V21.Lib;
+namespace Tyuiu.ShaykhelislamovaDA.Sprint5.Task6.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выолнила: Шайхелисламова Д. А. | ПКТБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Двумерные массивы  (статический ввод)                                   *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнила: Шайхелисламова Диана Артуровна | ПКТБ-25-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 3 до 6. Найдите сумму нечетных   *");
+            Console.WriteLine("* элементов массива.                                                      *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask6V21.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.LoadFromDataFile(path));
+        }
+    }
+}
